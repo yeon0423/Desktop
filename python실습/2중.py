8bfa3c5889e5c6d78eaed018051871285b85fc6b
yeon0423 <hong0404ha@naver.com>
2025-04-10 19:54:09 +0900

    rhkwp

--- python실습/2중.py
+++ python실습/2중.py
@@ -2,7 +2,6 @@ for dan in range(2, 10):
     print(f"# {dan}단 #", end="   ")  
 print()  
 
-# 본문 출력
 for i in range(1, 10):
     for dan in range(2, 10):
         print(f"# {dan} * {i} = {dan*i:>2} #", end="   ")  # 수식 출력
